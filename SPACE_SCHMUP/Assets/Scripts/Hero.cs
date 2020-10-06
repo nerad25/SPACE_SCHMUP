6a35714ede52b8b0a2451b6c773051660f25162d
nerad25 <dmcrull@yahoo.com>
2020-10-06 15:43:45 -0500

    Game set up and Hero creation

---
+++ SPACE_SCHMUP/Assets/Scripts/Hero.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S; //singleton
+
+    [Header("Set in Inspector")]
+    //These fields control movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float sheildLevel = 1;
+
+    private void Awake()
+    {
+        if (S == null)
+        {
+            S = this;   //set the singleton
+        }
+        else 
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis*pitchMult,xAxis*rollMult, 0);
+    }
+}
