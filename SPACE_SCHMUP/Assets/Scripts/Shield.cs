852406c75da39ee2471f6c9105344545c59a628a
nerad25 <dmcrull@yahoo.com>
2020-10-12 11:30:00 -0500

    Created shield and enemies
    Added script and prefabs for enemies and shield

---
+++ SPACE_SCHMUP/Assets/Scripts/Shield.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float rotationsPerSecond = 0.1f;
+
+    [Header("Set Dynamically")]
+    public int levelShown = 0;
+
+    //This non public variable will not appear in theInspector
+    Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Read current shield levels from Hero singleton
+        int currLevel = Mathf.FloorToInt(Hero.S.sheildLevel);
+
+        //If this is different from levelShown
+        if (levelShown != currLevel)
+        {
+            levelShown = currLevel;
+            // Adjust the texture offset to show different shield level
+            mat.mainTextureOffset = new Vector2(0.2f * levelShown, 0);
+        }
+
+        //Rotate the sield a bit every frame in a time based way
+        float rZ = -(rotationsPerSecond * Time.time * 360) % 360f;
+        transform.rotation = Quaternion.Euler(0, 0, rZ);
+    }
+}
