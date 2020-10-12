46db7a39c8a14f9dd09cddccd617a257fb551aeb
nerad25 <dmcrull@yahoo.com>
2020-10-12 12:58:21 -0500

    Added spawn enemies at random

---
+++ SPACE_SCHMUP/Assets/Scripts/Main.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    static public Main S;
+
+    [Header("Set in Inspector")]
+    public GameObject[] prefabEnemies;                  //Array for enemies
+    public float enemySpawnPerSecond = 0.5f;            // num of enemies per second
+    public float enemyDefaultPadding = 1.5f;            //Padding for position
+
+    private BoundsCheck bndCheck;
+
+    private void Awake()
+    {
+        S = this;
+        //Set bnd check to reference the bounds check component on this gameObject
+        bndCheck = GetComponent<BoundsCheck>();
+        //Invoke spawn enemy once (in 2 seconds, based on default values)
+        Invoke("SpawnEnemy", 1f/enemySpawnPerSecond);
+    }
+
+    public void SpawnEnemy()
+    {
+        //Pick random Enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        //Position rhe enemy above the screen with a random x position
+        float enemyPadding = enemyDefaultPadding;
+        if (go.GetComponent<BoundsCheck>() != null)
+        {
+            enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyPadding;
+        float xMax = bndCheck.camWidth - enemyPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyPadding;
+        go.transform.position = pos;
+
+        Invoke("SpawnEnemy", 1f/enemySpawnPerSecond);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
